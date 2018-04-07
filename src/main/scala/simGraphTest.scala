0aef3659b950e96659018f3967c3d639889f2eb2
tcphw5 <tcphw5@mst.edu>
2018-04-07 03:24:55 -0500

    traj data -> cliques being output correctly! Now to test multi thread :0 TODO: delete old stuff and commented out code

--- src/main/scala/simGraphTest.scala
+++ src/main/scala/simGraphTest.scala
@@ -5,7 +5,10 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.broadcast
 import org.apache.spark.sql.Row
+import org.apache.spark.sql.Column
 import org.apache.spark.rdd.RDD
+
+import scala.collection.mutable
 //import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.functions.udf
@@ -15,24 +18,22 @@ import java.io
 import scala.util.control.Breaks._
 //import org.saddle._
 
+import simcompare._
+
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions.{array, collect_list, lit, when}
 
-//there are a lot of imports for random stuff... some
-//were added during testing random stuff and could probably be removed
 
-
-//this application object is what the entire program
+//this is basically like your class in java
 object SimpleApp {
   def main(args: Array[String]): Unit = {
 
 
-
+    //suppress log output for testing
     Logger.getLogger("org").setLevel(Level.ERROR)
     Logger.getLogger("akka").setLevel(Level.ERROR)
 
     val inPeople = "genPpl.csv"
-
 
 
 
@@ -53,6 +54,7 @@ object SimpleApp {
 
     pplDF.show()
 
+
     //how to make sure neighbors are counted with both IDs groups without having duplicates??
     val newDf = pplDF.select('personID as "df1ID", 'traj as "df1traj").crossJoin(pplDF).where('df1ID =!= 'personID)
 
@@ -64,71 +66,75 @@ object SimpleApp {
     //newDf.show()
     newerDF.show()
 
+    val javasimComp = simcompare.simCompareV2.simScorePair _
 
 
 
-    def tsimUDF(a:Any, b:Any, c:Any) : Float = {
-      val r = scala.util.Random
-      return r.nextFloat()
+    def simCompareCallerfunc(a:String, b:String, c:Int) : Double = {
+
+      //formats Dataframe content form into something that can be read by the Java sim compare
+      val p1 = a.slice(1,a.indexOf('0') - 2).replaceAll("\'","").split(",").map(_.toDouble)
+      val t1 = a.slice(a.indexOf('0'), a.lastIndexOf('0') + 2).replaceAll("\'","").split(",").map(_.toDouble)
+      val p2 = b.slice(1,b.indexOf('0') - 2).replaceAll("\'","").split(",").map(_.toDouble)
+      val t2 = b.slice(a.indexOf('0'), b.lastIndexOf('0') + 2).replaceAll("\'","").split(",").map(_.toDouble)
+
+      //return 0 if index has none in common
+
+      return javasimComp(p1,t1,p2,t2,c)
     }
 
-    val tsimComp = tsimUDF _
-    val tsimCompUDF = udf(tsimComp)
+    val simCompareCall = simCompareCallerfunc _
+    val simCompUDF = udf(simCompareCall)
 
 
-    val resultsdf = newerDF.withColumn("result", tsimCompUDF(newerDF("df1traj"), newerDF("traj"), newerDF("lvl")))
+    val resultsdf = newerDF.withColumn("result", simCompUDF(newerDF("df1traj"), newerDF("traj"), newerDF("lvl")))
+
+
+    resultsdf.show()
 
     val combineLvlsDf = resultsdf.groupBy("df1ID", "personID").agg(sum("result"))
 
-    val aboveThreshDf = combineLvlsDf.where(combineLvlsDf("sum(result)") > 1)
+    val aboveThreshDf = combineLvlsDf.where(combineLvlsDf("sum(result)") > .8)
 
     aboveThreshDf.show()
 
-    //collect neighbors not working
+    //collect neighbors not working in rare cases
 
     val neighborGroups = aboveThreshDf.groupBy("df1ID").agg(collect_list("personID"))
 
-    neighborGroups.show()
+    def manOf[T: Manifest](t: T): Manifest[T] = manifest[T]
 
-    /*
+    //val neighborGroupsArr = neighborGroups.takeAsList(neighborGroups.count().toInt)
+    val neighborGroupsDF = neighborGroups.toDF()
+    neighborGroupsDF.show()
+    val neighborGroupsArrCol = neighborGroups.toDF().collect
+    //val neighborGroupsArrCol2 = neighborGroups.toDF().select("df1ID").map(r => r(0)).collect()
+    val neighborGroupsArr = neighborGroupsArrCol.map( r => Array(r.getAs[Int](0), r.getAs[Array[Int]](1)) )
+    val Pset = neighborGroupsArr.flatMap(_.headOption).map(x => x.toString.toInt).toSet
+    //println(neighborGroupsArr.toString())
 
     //reading in from files using spark
-    val simDF = spark.read
-                       .format("csv")
-                       .option("header", "true")
-                       .load(inFile)
+    //val simDF = spark.read
+    //                   .format("csv")
+    //                   .option("header", "true")
+    //                   .load(inFile)
 
-    val userDF = spark.read
-                      .format("csv")
-                      .option("header", "true")
-                      .load(inFile2)
+    //val userDF = spark.read
+    //                  .format("csv")
+    //                  .option("header", "true")
+    //                  .load(inFile2)
 
-    val simRDD: RDD[Row] = simDF.rdd
-
-
-    //test user defined function that combines rows as strings to the output row
-    def testudf(a:Any, b:Any) : Int = {
-      val stringA = a.toString.trim
-      val stringB = b.toString.trim
-      val combo = Integer.parseInt(stringA) + Integer.parseInt(stringB)
-      return combo
-    }
+    //val simRDD: RDD[Row] = simDF.rdd
 
 
-    //to declare function as a spark userdefined function
-    val testF = testudf _
-    val testUDF = udf(testF)
-
-    //sends col 0 and col 1 to the test udf as a map (can ignore)
-    val mappedRDD = simRDD.map(row => testudf(row.get(0), row.get(1)))
-    //creates a new column in the dataframe by sending the src and dst cols
-    val udfDF = simDF.withColumn("test", testUDF(simDF("src"), simDF("dst"))).show()
     //only shows rows where source is less than dest
-    val filtNeighborDF = simDF.filter($"src" < $"dst")
+    //val filtNeighborDF = simDF.filter($"src" < $"dst")
+
     //groups each source with all of its destinations
-    val neighborsDF = simDF.groupBy("src").agg(collect_list("dst")).show()
+    //val neighborsDF = simDF.groupBy("src").agg(collect_list("dst")).show()
+
     //same as last but with filtered neighbors list
-    val filtgrpNeighbors = filtNeighborDF.groupBy("src").agg(collect_list("dst")).show()
+    //val filtgrpNeighbors = filtNeighborDF.groupBy("src").agg(collect_list("dst")).show()
 
     //hard coded creating the neighbors table to be braodcasted at the moment
 
@@ -156,7 +162,8 @@ object SimpleApp {
     }
 
     //nbers2dmat2.foreach( x => println(x.mkString))
-    println(nbers2dmat3.deep.mkString("\n"))
+    // outputs entire neighbor array
+    //println(nbers2dmat3.deep.mkString("\n"))
     //println(nbers2dmat2(0).mkString)
     //println(nbers2dmat2(1)(0))
     //println(nbers2dmat2(0)(1))
@@ -176,14 +183,28 @@ object SimpleApp {
     nbers2dmat(4)(1) = Array(1,2)
     nbers2dmat(5)(1) = Array(1)
 
+    //neighborGroups.select(columns: _*).collect.map(_.toSeq)
+
+
+    val neighbors2 = spark.sparkContext.broadcast(neighborGroupsArr)
+    val Pbroad = spark.sparkContext.broadcast(Pset)
     val neighbors = spark.sparkContext.broadcast(nbers2dmat3)
+
+    //println(neighbors2.value)
+    //println(neighbors2.value(0)(0))
+    //println(neighbors2.value(1)(1))
+    val tests = neighbors2.value(1)(1).toString
+    val tests2 = tests.slice(13,tests.length-1).split(",").map(_.trim.toInt).toSet
+    //println(tests2.mkString)
+
 
     def bkcaller(cliqueStart:Any) : Int = {
       //Timing
       val t0 = System.nanoTime()
 
       val R = Set.empty[Int]
-      val P = (1 to 200).toSet
+      //val P = (1 to 200).toSet
+      val P = Pbroad.value
       val X = Set.empty[Int]
       val csString = cliqueStart.toString.trim
       val cliqueS = Integer.parseInt(csString)
@@ -200,12 +221,31 @@ object SimpleApp {
       return 5
     }
 
+
+    def findRow(searchInd:Int) : Int = {
+      val nSize = neighbors2.value.length
+      var rowInd = -1
+      for (i <- 0 until nSize) {
+        if (neighbors2.value(i)(0).toString == searchInd.toString()) {
+
+          rowInd = i
+          return rowInd
+        }
+      }
+
+      return rowInd
+    }
+
+
+    //rintln(neighbors2.value.get(0)(1).asInstanceOf[Array[Int]])
+    //println(neighbors2.value.get(findRow(3)))
+
     def bkCliqueFinder(R:Set[Int], P:Set[Int], X:Set[Int], cliqueS:Int, writer: PrintWriter) : Int = {
 
       if (P.size == 0 && X.size == 0 && R.contains(cliqueS)) {
         if (R.size > 2) {
           println("Clique: " + R.toString())
-          writer.write(R.toString())
+          writer.write(R.toString() + "\n")
         }
       }
       else {
@@ -214,7 +254,13 @@ object SimpleApp {
         var Xcopy = X.map(x => x)
 
         for (vertex <- Pcopy) {
-          val neighbs = neighbors.value(vertex-1)(1)
+          //val neighbs2 = neighbors2.value.get(findRow(vertex))(1)
+
+          //println(neighbs2)
+
+          val neighbsStr = neighbors2.value(findRow(vertex))(1).toString
+          val neighbs = neighbsStr.slice(13,neighbsStr.length-1).split(",").map(_.trim.toInt).toSeq
+          //println(neighbs.toString())
 
           bkCliqueFinder(R + vertex, Pcopy.intersect(Set(neighbs: _*)), Xcopy.intersect(Set(neighbs: _*)), cliqueS, writer)
           Pcopy -= vertex
@@ -229,7 +275,7 @@ object SimpleApp {
     }
 
 
-
+    /*
     //actual clique finding function
     //printlns inside used for testing
     def cliqueFinder(cliqueStart:Any) : Array[Int] = {
@@ -252,39 +298,6 @@ object SimpleApp {
       return arr
     }
 
-    /*def cliqueFinderRec(cliqueStart:Any) : Array[Int] = {
-      val csString = cliqueStart.toString.trim
-      val cliqueS = Integer.parseInt(csString)
-      var cliqueArr = ArrayBuffer[Int]()
-      cliqueArr += cliqueS
-      var counter = 1
-      val maxN = 4
-      var flag = true
-      val neighborRow = neighbors.value(cliqueS - 1)
-      for (neighbor <- neighborRow(1)) {
-        //if (neighbor > cliqueS) {
-        println("current nb " + neighbor)
-        flag = true
-        for (i <- 0 to counter - 1) {
-          println("if " + neighbors.value(neighbor - 1)(1).deep.mkString(", ") + " contains " + cliqueArr(i))
-          if (neighbors.value(neighbor - 1)(1).contains(cliqueArr(i))) {
-            // do nothing
-          } else {
-            flag = false
-          }
-        }
-        if (flag == true) {
-          cliqueArr += neighbor
-          counter += 1
-        }
-        //}
-      }
-
-      println(cliqueArr.toString())
-
-      return cliqueArr.sorted.toArray
-
-    }*/
 
     def cliqueFinderRec(cliqueStart:Any, counter:Int, cliqueArr:ArrayBuffer[Int], writer: PrintWriter) : Array[Int] = {
       val csString = cliqueStart.toString.trim
@@ -353,17 +366,18 @@ object SimpleApp {
       return cliqueArr.sorted.toArray
 
     }
+    */
 
-    val cftest = cliqueFinder _
+    //val cftest = cliqueFinder _
     val cftest2 = bkcaller _
 
+    //val cfUDF = udf(cftest)
     val cfUDF2 = udf(cftest2)
 
-    val cfUDF = udf(cftest)
     // make and show dataframe with column showing the clique starting from each node
-    val ansDF = userDF.withColumn("test", cfUDF(userDF("UID")))
+    //val ansDF = userDF.withColumn("test", cfUDF(userDF("UID")))
 
-    val ansDF2 = userDF.withColumn("test2", cfUDF2(userDF("UID")) )
+    val ansDF2 = neighborGroupsDF.withColumn("test2", cfUDF2(neighborGroupsDF("df1ID")) )
 
     //ansDF.show()
     ansDF2.show()
@@ -371,7 +385,7 @@ object SimpleApp {
     ////
     //END OF COMMENTED OUT OLD PROGRAM
     ////
-    */
+
 
     spark.stop()
   }
